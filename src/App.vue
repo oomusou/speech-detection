645ca9c2565b2a74e9d03a900042efc53f9454e1
oomusou <oomusou@gmail.com>
2024-01-30 14:55:30 +0800

    First release

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
-  <button @click="onStart">Start</button>
-  <button @click="onStop">Stop</button>
+  <div>Speech Detection Demo</div>
+  <div>
+    <button @click="onStart">Start</button>
+    <button @click="onStop">Stop</button>
+  </div>
+  <div>Threshold：<input type="text" v-model.number="threshold" /></div>
+  <div>avgVolume: {{ avgVolume }}</div>
+  <div>isSpeaking: {{ isSpeaking }}</div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { MediaRecorder } from 'extendable-media-recorder'
+
+let threshold = ref(50)
+let avgVolume = ref(0)
+let isSpeaking = ref(false)
 
 let mediaRecorder = null
 let chunks = []
 
 let onStart = async () => {
   const sampleRate = 16000
-  const period = 1000
-  const fileName = 'sample.wav'
+  const sampleInterval = 250 // 0.5s
 
   try {
     let mediaStream = await navigator.mediaDevices.getUserMedia({
@@ -20,11 +30,14 @@ let onStart = async () => {
     })
 
     let audioContext = new AudioContext({ sampleRate })
+    let analyser = audioContext.createAnalyser()
+    analyser.fftSize = 256
 
     let srcNode = new MediaStreamAudioSourceNode(audioContext, { mediaStream })
     let destNode = new MediaStreamAudioDestinationNode(audioContext)
 
-    srcNode.connect(destNode)
+    srcNode.connect(analyser)
+    analyser.connect(destNode)
 
     mediaRecorder = new MediaRecorder(destNode.stream, {
       mimeType: 'audio/wav'
@@ -32,20 +45,16 @@ let onStart = async () => {
 
     mediaRecorder.ondataavailable = (e) => {
       chunks.push(e.data)
+
+      const bufferLength = analyser.frequencyBinCount
+      let dataArray = new Uint8Array(bufferLength)
+      analyser.getByteFrequencyData(dataArray)
+
+      avgVolume.value = dataArray.reduce((sum, value) => sum + value, 0) / bufferLength
+      isSpeaking.value = avgVolume.value > threshold.value
     }
 
-    mediaRecorder.onstop = () => {
-      let blob = new Blob(chunks)
-
-      let link = document.createElement('a')
-      link.href = URL.createObjectURL(blob)
-      link.download = fileName
-      link.click()
-
-      URL.revokeObjectURL(link.href)
-    }
-
-    mediaRecorder.start(period)
+    mediaRecorder.start(sampleInterval)
   } catch (err) {
     console.warn(err)
   }
